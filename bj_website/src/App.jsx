b84e2a4423fef753862eb99569ffa68710db62ab
dxg7905 <dxg7905@mavs.uta.edu>
2024-08-26 15:43:27 -0500

    first edit

---
+++ bj_website/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Hero from './Components/Hero'
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+       
+        <Hero/>
+      
+      </div>
+
+  )
+}
+
+export default App
